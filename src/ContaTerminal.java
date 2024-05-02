a133fa215febd424b806ca850cfe53a0ac60d7d2
hugobor <hugoborgescarvalho@gmail.com>
2024-05-02 16:27:56 -0300

    Projeto criado

---
+++ src/ContaTerminal.java
@@ -0,0 +1,30 @@
+
+/**
+ * Classe do coiso do terminal.
+ */
+public class ContaTerminal {
+    
+    public static void main(String[] args) {
+	
+	System.out.println("OIEEEE!!!!");
+	
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
